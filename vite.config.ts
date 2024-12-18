e72794f883c8c57cb12733773913283f16c998a4
aucklandcodernz <59552014+aucklandcodernz@users.noreply.github.com>
2024-12-18 04:37:40 +0000

    Restored to '6a64878d32ed71c4a02759acc57b8e22e80ff541'
    Replit-Restored-To:6a64878d32ed71c4a02759acc57b8e22e80ff541

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -9,10 +9,6 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3000,
-    hmr: {
-      clientPort: 443,
-      host: '0.0.0.0'
-    }
+    port: 3000
   }
 });
